7aa605dce36d8b9a31b09c063664698150994099
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-09-23 00:01:50 +0200

    added all playerXXSlotXX to listener events with imgs.

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ gridJugador01Casilla01.addEventListener('mouseleave', () => {
 //Guardo gridPlayer01Slot02 en una constante
 const gridJugador01Casilla02 = document.getElementById('gridPlayer01Slot02');
 
-//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer01Slot01
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer01Slot02
 gridJugador01Casilla02.addEventListener('mouseenter', () => {
     document.getElementById('imgPlayer01').classList.add('imgPlayer01Slot02');
     document.getElementById('statsPlayer01').classList.add('statsPlayer01Slot02');
@@ -43,7 +43,98 @@ gridJugador01Casilla02.addEventListener('mouseleave', () => {
     document.getElementById('statsPlayer01').classList.remove('statsPlayer01Slot02')
 })
 
-//TENGO QUE PROBAR SI CON EL CLICK SIGUE HACIENDO EFECTO EL MOUSEENTER
-//ANTES DE METERLE MOUSEENTER A TODAS LOS PLAYERS
-//ES DECIR, QUE AL SELECCIONAR UN PLAYER, SI PASO EL RATON POR ENCIMA DE OTRO
-//ÉSTE SIGUE RECIBIENDO LOS EFECTOS DE PASAR EL RATON POR ENCIMA
+
+//Guardo gridPlayer01Slot03 en una constante
+const gridJugador01Casilla03 = document.getElementById('gridPlayer01Slot03');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer01Slot03
+gridJugador01Casilla03.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer01').classList.add('imgPlayer01Slot03');
+    document.getElementById('statsPlayer01').classList.add('statsPlayer01Slot03');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador01Casilla03.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer01').classList.remove('imgPlayer01Slot03')
+    document.getElementById('statsPlayer01').classList.remove('statsPlayer01Slot03')
+})
+
+
+//Guardo gridPlayer01Slot04 en una constante
+const gridJugador01Casilla04 = document.getElementById('gridPlayer01Slot04');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer01Slot04
+gridJugador01Casilla04.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer01').classList.add('imgPlayer01Slot04');
+    document.getElementById('statsPlayer01').classList.add('statsPlayer01Slot04');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador01Casilla04.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer01').classList.remove('imgPlayer01Slot04')
+    document.getElementById('statsPlayer01').classList.remove('statsPlayer01Slot04')
+})
+
+
+//Guardo gridPlayer02Slot01 en una constante
+const gridJugador02Casilla01 = document.getElementById('gridPlayer02Slot01');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer02Slot01
+gridJugador02Casilla01.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer02').classList.add('imgPlayer02Slot01');
+    document.getElementById('statsPlayer02').classList.add('statsPlayer02Slot01');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador02Casilla01.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer02').classList.remove('imgPlayer02Slot01')
+    document.getElementById('statsPlayer02').classList.remove('statsPlayer02Slot01')
+})
+
+
+//Guardo gridPlayer02Slot02 en una constante
+const gridJugador02Casilla02 = document.getElementById('gridPlayer02Slot02');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer02Slot02
+gridJugador02Casilla02.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer02').classList.add('imgPlayer02Slot02');
+    document.getElementById('statsPlayer02').classList.add('statsPlayer02Slot02');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador02Casilla02.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer02').classList.remove('imgPlayer02Slot02')
+    document.getElementById('statsPlayer02').classList.remove('statsPlayer02Slot02')
+})
+
+
+//Guardo gridPlayer02Slot03 en una constante
+const gridJugador02Casilla03 = document.getElementById('gridPlayer02Slot03');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer02Slot03
+gridJugador02Casilla03.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer02').classList.add('imgPlayer02Slot03');
+    document.getElementById('statsPlayer02').classList.add('statsPlayer02Slot03');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador02Casilla03.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer02').classList.remove('imgPlayer02Slot03')
+    document.getElementById('statsPlayer02').classList.remove('statsPlayer02Slot03')
+})
+
+
+//Guardo gridPlayer02Slot04 en una constante
+const gridJugador02Casilla04 = document.getElementById('gridPlayer02Slot04');
+
+//Construyo los eventos que tienen lugar al pasar el mouse por encima de gridPlayer02Slot04
+gridJugador02Casilla04.addEventListener('mouseenter', () => {
+    document.getElementById('imgPlayer02').classList.add('imgPlayer02Slot04');
+    document.getElementById('statsPlayer02').classList.add('statsPlayer02Slot04');
+})
+
+//y estos eventos devuelven todo a su estado original al quitar el mouse de encima
+gridJugador02Casilla04.addEventListener('mouseleave', () => {
+    document.getElementById('imgPlayer02').classList.remove('imgPlayer02Slot04')
+    document.getElementById('statsPlayer02').classList.remove('statsPlayer02Slot04')
+})
